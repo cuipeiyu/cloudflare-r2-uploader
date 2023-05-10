7729bbae355cce78ba6011551ee7146c11d47bbc
CuiPeiyu <i@cuipeiyu.com>
2023-05-10 16:50:48 +0800

    use cobra

--- main.go
+++ main.go
@@ -14,115 +14,160 @@ import (
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
-func getEnv(key, fallback string) string {
-	if value, ok := os.LookupEnv(key); ok {
-		return strings.TrimSpace(value)
-	}
-	return fallback
-}
+var (
+	bucketName      = ""
+	accountId       = ""
+	accessKeyId     = ""
+	accessKeySecret = ""
+)
 
 func main() {
-	if len(os.Args) != 3 {
-		log.Fatalln("usage: cloudflare-r2-uploader <local-path> <remote-path>")
-		return
-	}
+	viper.SetEnvPrefix("CFR2")
+	viper.AutomaticEnv()
 
-	localPath := os.Args[1]
-	remotePath := strings.TrimLeft(os.Args[2], "/")
-
-	var bucketName = getEnv("CFR2_BUCKET", "")
-	var accountId = getEnv("CFR2_ACCOUNT_ID", "")
-	var accessKeyId = getEnv("CFR2_ACCESSKEY", "")
-	var accessKeySecret = getEnv("CFR2_SECRETKEY", "")
+	bucketName = viper.GetString("BUCKET")
+	accountId = viper.GetString("ACCOUNT_ID")
+	accessKeyId = viper.GetString("ACCESSKEY")
+	accessKeySecret = viper.GetString("SECRETKEY")
 
 	if bucketName == "" || accountId == "" || accessKeyId == "" || accessKeySecret == "" {
 		log.Fatalln("unknown cloudflare config")
 		return
 	}
 
-	r2Resolver := aws.EndpointResolverWithOptionsFunc(func(service, region string, options ...interface{}) (aws.Endpoint, error) {
-		return aws.Endpoint{
-			URL: fmt.Sprintf("https://%s.r2.cloudflarestorage.com", accountId),
-		}, nil
-	})
+	var rootCmd = &cobra.Command{Use: "cloudflare-r2-uploader"}
 
-	cfg, err := config.LoadDefaultConfig(context.TODO(),
-		config.WithEndpointResolverWithOptions(r2Resolver),
-		config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(accessKeyId, accessKeySecret, "")),
-	)
-	if err != nil {
-		log.Fatal(err)
+	rootCmd.AddCommand(uploadCmd())
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
+}
 
-	ctx, cancelFn := context.WithTimeout(context.Background(), time.Minute)
-	defer cancelFn()
+func uploadCmd() *cobra.Command {
+	upload := &cobra.Command{
+		Use:              "upload",
+		Short:            "upload",
+		Long:             "",
+		TraverseChildren: true,
+		Args:             cobra.MinimumNArgs(2),
+		Run: func(cmd *cobra.Command, args []string) {
+			force, _ := cmd.Flags().GetBool("force")
 
-	client := s3.NewFromConfig(cfg)
+			localPath := args[0]
+			remotePath := strings.TrimLeft(args[1], "/")
 
-	log.Printf("upload \"%s\" to \"%s\"", localPath, remotePath)
+			r2Resolver := aws.EndpointResolverWithOptionsFunc(func(service, region string, options ...interface{}) (aws.Endpoint, error) {
+				return aws.Endpoint{
+					URL: fmt.Sprintf("https://%s.r2.cloudflarestorage.com", accountId),
+				}, nil
+			})
 
-	info, err := os.Stat(localPath)
-	if err != nil {
-		log.Fatalln(err)
-	}
+			cfg, err := config.LoadDefaultConfig(context.TODO(),
+				config.WithEndpointResolverWithOptions(r2Resolver),
+				config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(accessKeyId, accessKeySecret, "")),
+			)
+			if err != nil {
+				log.Fatal(err)
+			}
 
-	if info.IsDir() {
-		count := 0
+			ctx, cancelFn := context.WithTimeout(context.Background(), time.Minute)
+			defer cancelFn()
 
-		localPathAbs, _ := filepath.Abs(localPath)
+			client := s3.NewFromConfig(cfg)
 
-		filepath.Walk(localPathAbs, func(path string, info fs.FileInfo, err error) error {
+			log.Printf("upload \"%s\" to \"%s\"", localPath, remotePath)
+
+			info, err := os.Stat(localPath)
 			if err != nil {
 				log.Fatalln(err)
 			}
 
 			if info.IsDir() {
-				return nil // keep going
+				count := 0
+
+				localPathAbs, _ := filepath.Abs(localPath)
+
+				filepath.Walk(localPathAbs, func(path string, info fs.FileInfo, err error) error {
+					if err != nil {
+						log.Fatalln(err)
+					}
+
+					if info.IsDir() {
+						return nil // keep going
+					}
+
+					file, err := os.OpenFile(path, os.O_RDONLY, os.ModePerm)
+					if err != nil {
+						log.Fatalln(err)
+					}
+					defer file.Close()
+
+					key := strings.TrimPrefix(path, localPathAbs)
+					key = strings.TrimPrefix(filepath.Join(remotePath, key), "/")
+					log.Printf("uploading [% 4d] %s", count, key)
+
+					_, err = client.PutObject(ctx, &s3.PutObjectInput{
+						Bucket: aws.String(bucketName),
+						Key:    aws.String(key),
+						Body:   file,
+					})
+					if err != nil {
+						log.Fatalln(err)
+					}
+
+					count++
+
+					return nil
+				})
+
+				log.Printf("uploaded %d files", count)
+			} else {
+				skip := !force
+
+				if !force {
+					_, err = client.HeadObject(ctx, &s3.HeadObjectInput{
+						Bucket: aws.String(bucketName),
+						Key:    aws.String(remotePath),
+					})
+					if err != nil {
+						if strings.Contains(err.Error(), "Not Found") {
+							skip = false
+						}
+					}
+				}
+
+				if skip {
+					log.Printf("\"%s\" is exists will be skipped", remotePath)
+				} else {
+					file, err := os.OpenFile(localPath, os.O_RDONLY, os.ModePerm)
+					if err != nil {
+						log.Fatalln(err)
+					}
+					defer file.Close()
+
+					_, err = client.PutObject(ctx, &s3.PutObjectInput{
+						Bucket: aws.String(bucketName),
+						Key:    aws.String(remotePath),
+						Body:   file,
+					})
+					if err != nil {
+						log.Fatalln(err)
+					}
+				}
 			}
 
-			file, err := os.OpenFile(path, os.O_RDONLY, os.ModePerm)
-			if err != nil {
-				log.Fatalln(err)
-			}
-			defer file.Close()
-
-			key := strings.TrimPrefix(path, localPathAbs)
-			key = strings.TrimPrefix(filepath.Join(remotePath, key), "/")
-			log.Printf("uploading [% 4d] %s", count, key)
-
-			_, err = client.PutObject(ctx, &s3.PutObjectInput{
-				Bucket: aws.String(bucketName),
-				Key:    aws.String(key),
-				Body:   file,
-			})
-			if err != nil {
-				log.Fatalln(err)
-			}
-
-			count++
-
-			return nil
-		})
-
-		log.Printf("uploaded %d files", count)
-	} else {
-		file, err := os.OpenFile(localPath, os.O_RDONLY, os.ModePerm)
-		if err != nil {
-			log.Fatalln(err)
-		}
-		defer file.Close()
-
-		_, err = client.PutObject(ctx, &s3.PutObjectInput{
-			Bucket: aws.String(bucketName),
-			Key:    aws.String(remotePath),
-			Body:   file,
-		})
-		if err != nil {
-			log.Fatalln(err)
-		}
+			log.Println("complete")
+		},
 	}
 
-	log.Println("complete")
+	// force upload
+	upload.Flags().Bool("force", true, "Force upload even if the file exists.")
+
+	return upload
 }
